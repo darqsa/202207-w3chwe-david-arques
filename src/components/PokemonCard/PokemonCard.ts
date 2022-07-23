37fe00cdb39c36bcb3f6d65c798387d68a088b91
David Arques <david.darqsa@gmail.com>
2022-07-23 21:34:32 +0200

    Linked pokemon image, title and stats to html

---
+++ src/components/PokemonCard/PokemonCard.ts
@@ -0,0 +1,42 @@
+import pokemonStats from "../../modules/pokemon/pokemon.js";
+
+const renderPokemonCard = async () => {
+  const pokemon = await pokemonStats(0);
+  const pokemonCardHeader = () => {
+    const pokemonName = document.createElement("h1");
+    document.querySelector(".card-container__header").appendChild(pokemonName);
+    pokemonName.textContent = `#${pokemon.id} - ${pokemon.name}`;
+    pokemonName.className = "card-container__pokemon-name";
+  };
+  const cardContainer = ".card-container__container";
+  const pokemonCardImages = () => {
+    const pokemonSprite = document.createElement("img");
+    document
+      .querySelector(".card-container__images-container")
+      .appendChild(pokemonSprite);
+    pokemonSprite.className = "card-container__image";
+    pokemonSprite.src = `${pokemon.sprite}`;
+  };
+
+  const pokemonCardStats = () => {
+    const typeStat = document.createElement("span");
+    document.querySelector("#stat1").appendChild(typeStat);
+    typeStat.textContent = `Types: ${pokemon.type}`;
+
+    const heightStat = document.createElement("span");
+    document.querySelector("#stat2").appendChild(heightStat);
+    heightStat.textContent = `Height: ${pokemon.height}`;
+
+    const weightStat = document.createElement("span");
+    document.querySelector("#stat3").appendChild(weightStat);
+    weightStat.textContent = `Weight: ${pokemon.weight}`;
+
+    const abilitiesStat = document.createElement("span");
+    document.querySelector("#stat4").appendChild(abilitiesStat);
+    abilitiesStat.textContent = `Abilities: ${pokemon.abilities}`;
+  };
+  pokemonCardHeader();
+  pokemonCardImages();
+  pokemonCardStats();
+};
+export default renderPokemonCard;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import renderPokemonCard from "./components/PokemonCard/PokemonCard.js";
+
 import currentPokemon from "./modules/apiUrls/currentPokemonUrl.js";
 import pokemonStats from "./modules/pokemon/pokemon.js";
 
 // document.querySelector(".card-container__pokemon-name").textContent =
 currentPokemon(4);
-pokemonStats("hola", 0);
+pokemonStats(0);
+renderPokemonCard();
