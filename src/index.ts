382f89205cbea5d7c1517260b092e27cf2d0305f
David Arques <david.darqsa@gmail.com>
2022-07-24 00:55:05 +0200

    Solved audit extension in index.ts issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// document.querySelector(".card-container__pokemon-name").textContent =
-
 import renderPokemonCard from "./components/pokemonCard/pokemonCard.js";
 import renderPokemonSelector from "./components/pokemonSelector/pokemonSelector.js";
 
